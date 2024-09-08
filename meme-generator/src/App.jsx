a6426af991f9a9f24b151e63faf39176ddff73cf
camdentadhg <camdent@gmail.com>
2024-09-08 12:52:49 -0500

    basic meme generator complete

--- meme-generator/src/App.jsx
+++ meme-generator/src/App.jsx
@@ -15,10 +15,6 @@ function App() {
 
 export default App
 
-//add a delete button
-//create dispatch for deleting a meme
-//delete a meme
-
 //add unit test to reducer
 //add functionality for updating memes
 //store memes in local storage
--- meme-generator/src/Meme.jsx
+++ meme-generator/src/Meme.jsx
@@ -1,13 +1,20 @@
 import React from 'react';
+import {useDispatch} from 'react-redux';
 import './Meme.css';
 
 const Meme = ({meme}) => {
+    const dispatch = useDispatch();
+
+    const handleDelete = (event) => {
+        dispatch({type: 'DELETE', payload: event.target.id});
+    }
+
     return (
         <div class='meme-wrapper'>
             <span class='topText'>{meme.topText}</span>
             <img src={meme.img}/>
             <span class='bottomText'>{meme.bottomText}</span>
-            <button id={meme.id}>X</button>
+            <button id={meme.id} class="delete" onClick={handleDelete}>X</button>
         </div>
     )
 }
